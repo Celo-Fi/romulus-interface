3f4839c7c9f2b95540e9dabbf5746d26a2799874
Ian Macalinao <ian@ubeswap.org>
2021-07-22 13:07:25 -0400

    more pools

--- src/components/pages/ubeswap/UbeswapAdmin/AllocatePools.tsx
+++ src/components/pages/ubeswap/UbeswapAdmin/AllocatePools.tsx
@@ -23,7 +23,7 @@ export const AllocatePools: React.FC = () => {
           const signer = await getConnectedSigner();
           const newPools = [
             // UBE-CELO
-            // ["0xe7b5ad135fa22678f426a381c7748f6a5f2c9e6c", 76],
+            ["0xe7b5ad135fa22678f426a381c7748f6a5f2c9e6c", 64],
             // UBE-MCUSD
             // ["0x59b22100751b7fda0c88201fb7a0eaf6fc30bcc7", 0],
             // UBE-mcEUR
@@ -39,11 +39,15 @@ export const AllocatePools: React.FC = () => {
             // cBTC-MCUSD
             // ["0x83CF02F79Be87A7402A3Cac013d0e1C95FeFcAba", 6],
             // POOF-UBE
-            // ["0x573bcebd09ff805ed32df2cb1a968418dc74dcf7", 1],
+            ["0x573bcebd09ff805ed32df2cb1a968418dc74dcf7", 7],
             // rCELO-CELO
-            // ["0x58fff7110e39c733fd37742b8850f9205fbc351b", 1],
+            ["0x58fff7110e39c733fd37742b8850f9205fbc351b", 3],
             // cETH-CELO
-            ["0xc0864bec4e878371d2bfb1e92928e30a2fe91cf1", 0],
+            // ["0xc0864bec4e878371d2bfb1e92928e30a2fe91cf1", 0],
+            // cETH-mcUSD
+            ["0xb5108b01280f994e67dc8bc3cd1e2433fa3a1b41", 6],
+            // MOO-mCELO
+            ["0x69d5646e63C7cE63171F76EBA89348b52c1D552c", 2],
           ] as const;
 
           const tx = await poolManager.connect(signer).batchSetWeight(
