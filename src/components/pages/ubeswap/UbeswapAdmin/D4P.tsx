7b8caeb6cd3be58675e173ac2bc49ff091ca2ff7
Brian Li <brnli7@gmail.com>
2021-10-21 11:57:42 -0700

    Refactor D4P controls to use ABI encoding

--- src/components/pages/ubeswap/UbeswapAdmin/D4P.tsx
+++ src/components/pages/ubeswap/UbeswapAdmin/D4P.tsx
@@ -1,39 +1,65 @@
 import { useGetConnectedSigner } from "@celo-tools/use-contractkit";
 import React from "react";
-import { Button } from "theme-ui";
+import { Button, Flex } from "theme-ui";
 import { useMultisigContract } from "../../../../hooks/useMultisigContract";
+import ERC20Abi from "../../../../abis/ERC20.json";
+import MSRAbi from "../../../../abis/MoolaStakingRewards.json";
+import { AbiItem, toWei } from "web3-utils";
+import Web3 from "web3";
+const web3 = new Web3();
 
 enum Pool {
+  // Ubeswap controlled
   CELOUSD,
   CELOEUR,
   CELOUBE,
   CELORCELO,
+
+  // Poof controlled
 }
 
-const calls = {
+const transferInterface = ERC20Abi.find((f) => f.name === "transfer");
+const getTransferData = (recipient: string, amount: string) =>
+  transferInterface
+    ? web3.eth.abi.encodeFunctionCall(transferInterface as AbiItem, [
+        recipient,
+        amount,
+      ])
+    : null;
+const notifyInterface = MSRAbi.find((f) => f.name === "notifyRewardAmount");
+const getNotifyData = (amount: string) =>
+  notifyInterface
+    ? web3.eth.abi.encodeFunctionCall(notifyInterface as AbiItem, [amount])
+    : null;
+
+type Farm = {
+  farmAddress: string;
+  rewardToken: string;
+  amount: string;
+};
+
+const farms: Record<Pool, Farm> = {
   [Pool.CELOUSD]: {
-    send: "0xa9059cbb000000000000000000000000bbc8c824c638fd238178a71f5b1e5ce7e4ce586b0000000000000000000000000000000000000000000000c77e4256863d800000",
-    farm: "0xbbC8C824c638fd238178a71F5b1E5Ce7e4Ce586B",
-    notify:
-      "0x3c6b16ab0000000000000000000000000000000000000000000000c77e4256863d800000",
+    // farm: "0xbbC8C824c638fd238178a71F5b1E5Ce7e4Ce586B", // OLD
+    farmAddress: "0x161c77b4919271B7ED59AdB2151FdaDe3F907a1F",
+    rewardToken: "0x471ece3750da237f93b8e339c536989b8978a438",
+    amount: toWei("3680"),
   },
   [Pool.CELOEUR]: {
-    send: "0xa9059cbb0000000000000000000000000f3d01aea89da0b6ad81712edb96fa7af1c17e9b0000000000000000000000000000000000000000000000c77e4256863d800000",
-    farm: "0x0F3d01aea89dA0b6AD81712Edb96FA7AF1c17E9B",
-    notify:
-      "0x3c6b16ab0000000000000000000000000000000000000000000000c77e4256863d800000",
+    // farm: "0x0F3d01aea89dA0b6AD81712Edb96FA7AF1c17E9B", // OLD
+    farmAddress: "0x728C650D1Fb4da2D18ccF4DF45Af70c5AEb09f81",
+    rewardToken: "0x471ece3750da237f93b8e339c536989b8978a438",
+    amount: toWei("3680"),
   },
   [Pool.CELOUBE]: {
-    send: "0xa9059cbb0000000000000000000000009d87c01672a7d02b2dc0d0eb7a145c7e13793c3b00000000000000000000000000000000000000000000004acf58e07257100000",
-    farm: "0x9D87c01672A7D02b2Dc0D0eB7A145C7e13793c3B",
-    notify:
-      "0x3c6b16ab00000000000000000000000000000000000000000000004acf58e07257100000",
+    farmAddress: "0x9D87c01672A7D02b2Dc0D0eB7A145C7e13793c3B",
+    rewardToken: "0x471ece3750da237f93b8e339c536989b8978a438",
+    amount: toWei("1380"),
   },
   [Pool.CELORCELO]: {
-    send: "0xa9059cbb000000000000000000000000194478aa91e4d7762c3e51eee57376ea9ac72761000000000000000000000000000000000000000000000018e1e7941d204c0000",
-    farm: "0x194478Aa91e4D7762c3E51EeE57376ea9ac72761",
-    notify:
-      "0x3c6b16ab000000000000000000000000000000000000000000000018e1e7941d204c0000",
+    farmAddress: "0x194478Aa91e4D7762c3E51EeE57376ea9ac72761",
+    rewardToken: "0x471ece3750da237f93b8e339c536989b8978a438",
+    amount: toWei("459"),
   },
 };
 
@@ -45,59 +71,64 @@ export const D4P = () => {
   const sendCELO = React.useCallback(
     async (pool: Pool) => {
       const signer = await getConnectedSigner();
-      await multisig
-        .connect(signer as any)
-        .submitTransaction(
-          "0x471ece3750da237f93b8e339c536989b8978a438",
-          0,
-          calls[pool].send
-        );
+      const data = getTransferData(farms[pool].farmAddress, farms[pool].amount);
+      if (data) {
+        await multisig
+          .connect(signer as any)
+          .submitTransaction(farms[pool].rewardToken, 0, data);
+      }
     },
     [multisig, getConnectedSigner]
   );
   const notify = React.useCallback(
     async (pool: Pool) => {
       const signer = await getConnectedSigner();
-      await multisig
-        .connect(signer as any)
-        .submitTransaction(calls[pool].farm, 0, calls[pool].notify);
+      const data = getNotifyData(farms[pool].amount);
+      if (data) {
+        await multisig
+          .connect(signer as any)
+          .submitTransaction(farms[pool].farmAddress, 0, data);
+      }
     },
     [multisig, getConnectedSigner]
   );
   return (
     <div>
-      <Button onClick={() => sendCELO(Pool.CELOUSD)}>
-        Transfer CELO to CELO-mcUSD
-      </Button>
+      <Flex>
+        <Button onClick={() => sendCELO(Pool.CELOUSD)} m={1}>
+          Transfer CELO to CELO-mcUSD
+        </Button>
+        <Button onClick={() => notify(Pool.CELOUSD)} m={1}>
+          Notify CELO to CELO-mcUSD
+        </Button>
+      </Flex>
       <br />
-      <Button onClick={() => sendCELO(Pool.CELOEUR)}>
-        Transfer CELO to CELO-mcEUR
-      </Button>
+      <Flex>
+        <Button onClick={() => sendCELO(Pool.CELOEUR)} m={1}>
+          Transfer CELO to CELO-mcEUR
+        </Button>
+        <Button onClick={() => notify(Pool.CELOEUR)} m={1}>
+          Notify CELO to CELO-mcEUR
+        </Button>
+      </Flex>
       <br />
-      <Button onClick={() => sendCELO(Pool.CELOUBE)}>
-        Transfer CELO to UBE-CELO
-      </Button>
+      <Flex>
+        <Button onClick={() => sendCELO(Pool.CELOUBE)} m={1}>
+          Transfer CELO to UBE-CELO
+        </Button>
+        <Button onClick={() => notify(Pool.CELOUBE)} m={1}>
+          Notify CELO to UBE-CELO
+        </Button>
+      </Flex>
       <br />
-      <Button onClick={() => sendCELO(Pool.CELORCELO)}>
-        Transfer CELO to rCELO-CELO
-      </Button>
-      <br />
-
-      <Button onClick={() => notify(Pool.CELOUSD)}>
-        Notify CELO to CELO-mcUSD
-      </Button>
-      <br />
-      <Button onClick={() => notify(Pool.CELOEUR)}>
-        Notify CELO to CELO-mcEUR
-      </Button>
-      <br />
-      <Button onClick={() => notify(Pool.CELOUBE)}>
-        Notify CELO to UBE-CELO
-      </Button>
-      <br />
-      <Button onClick={() => notify(Pool.CELORCELO)}>
-        Notify CELO to rCELO-CELO
-      </Button>
+      <Flex>
+        <Button onClick={() => sendCELO(Pool.CELORCELO)} m={1}>
+          Transfer CELO to rCELO-CELO
+        </Button>
+        <Button onClick={() => notify(Pool.CELORCELO)} m={1}>
+          Notify CELO to rCELO-CELO
+        </Button>
+      </Flex>
     </div>
   );
 };
