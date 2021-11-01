9855d26c52d0cb4e25a66ac6db22ca46587e333a
Brian Li <brnli7@gmail.com>
2021-11-01 18:12:33 -0400

    Delete index.tsx

--- src/pages/readcontract/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { ContractCall } from "../../components/pages/contracts/ContractCall";
-
-const ContractCallPage: React.FC = () => {
-  return <ContractCall />;
-};
-
-export default ContractCallPage;
