90d11f26b5758342adc0e5cb9ab0794a59b6fd57
Brian Li <brnli7@gmail.com>
2021-11-25 11:59:33 -0500

    Fix toWei import

--- src/components/pages/ubeswap/UbeswapAdmin/D4P.tsx
+++ src/components/pages/ubeswap/UbeswapAdmin/D4P.tsx
@@ -6,7 +6,7 @@ import moment from "moment";
 import React from "react";
 import { Box, Button, Card, Flex, Heading, Link, Text } from "theme-ui";
 import Web3 from "web3";
-import { AbiItem, fromWei } from "web3-utils";
+import { AbiItem, fromWei, toWei } from "web3-utils";
 
 import ERC20Abi from "../../../../abis/ERC20.json";
 import MSRAbi from "../../../../abis/MoolaStakingRewards.json";
