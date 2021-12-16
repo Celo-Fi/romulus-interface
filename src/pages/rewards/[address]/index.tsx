6f06fabe2906656e5760a0c04f824583f8763d66
Brian Li <brnli7@gmail.com>
2021-12-15 19:24:12 -0800

    Delete unused in rewards page

--- src/pages/rewards/[address]/index.tsx
+++ src/pages/rewards/[address]/index.tsx
@@ -2,82 +2,19 @@ import {
   useGetConnectedSigner,
   useProvider,
 } from "@celo-tools/use-contractkit";
-import { ethers } from "ethers";
 import moment from "moment";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { Box, Button, Card, Flex, Heading, Link, Text } from "theme-ui";
-import Web3 from "web3";
-import { AbiItem, fromWei, toWei } from "web3-utils";
+import { fromWei, toWei } from "web3-utils";
 
-import ERC20Abi from "../../../abis/ERC20.json";
-import MSRAbi from "../../../abis/MoolaStakingRewards.json";
 import { Address } from "../../../components/common/Address";
 import {
   ERC20__factory,
-  FarmRegistry__factory,
   IUniswapV2Pair__factory,
   MoolaStakingRewards__factory,
-  MultiSig as MultisigContract,
 } from "../../../generated";
 import { useAsyncState } from "../../../hooks/useAsyncState";
-import { useMultisigContract } from "../../../hooks/useMultisigContract";
-import { FARM_REGISTRY_ADDRESS } from "../../../hooks/useRegisteredFarms";
-
-const web3 = new Web3("https://forno.celo.org"); // TODO: HARDCODE
-
-// == UTILS ==
-const transferInterface = ERC20Abi.find((f) => f.name === "transfer");
-const getTransferData = (recipient: string, amount: string) =>
-  transferInterface
-    ? web3.eth.abi.encodeFunctionCall(transferInterface as AbiItem, [
-        recipient,
-        amount,
-      ])
-    : null;
-const notifyInterface = MSRAbi.find((f) => f.name === "notifyRewardAmount");
-const getNotifyData = (amount: string) =>
-  notifyInterface
-    ? web3.eth.abi.encodeFunctionCall(notifyInterface as AbiItem, [amount])
-    : null;
-
-// == TYPES ==
-enum Pool {
-  // Ubeswap controlled
-  CELOUSD = "CELO-mcUSD",
-  CELOEUR = "CELO-mcEUR",
-  CELOUBE = "UBE-CELO",
-  CELORCELO = "rCELO-CELO",
-  WBTCMCUSD = "WBTC-mcUSD",
-  WETHMCUSD = "WETH-mcUSD",
-  SUSHIMCUSD = "SUSHI-mcUSD",
-  CRVMCUSD = "CRV-mcUSD",
-  AAVEMCUSD = "AAVE-mcUSD",
-  FTMMCUSD = "FTM-mcUSD",
-  AVAXMCUSD = "AVAX-mcUSD",
-  WMATICMCUSD = "WMATIC-mcUSD",
-  BNBMCUSD = "BNB-mcUSD",
-  SOLCELO = "SOL-CELO",
-  UBE = "UBE",
-
-  // Poof controlled
-  POOFUBE = "POOF-UBE",
-  PCELOPOOF = "pCELO-POOF",
-  pUSDUSD = "pUSD-USDC-cUSD",
-  pEUREUR = "pEUR-cEUR",
-  pCELOCELO = "pCELO-CELO",
-}
-
-enum Token {
-  CELO = "0x471ece3750da237f93b8e339c536989b8978a438",
-  POOF = "0x00400fcbf0816bebb94654259de7273f4a05c762",
-  UBE = "0x00be915b9dcf56a3cbe739d9b9c202ca692409ec",
-}
-
-enum Multisig {
-  UBE = "0x0Ce41DbCEA62580Ae2C894a7D93E97da0c3daC3a",
-  POOF = "0x54c18437bC09Ee60BCd40aFe7E560010860fFC1F",
-}
 
 type Farm = {
   farmAddress: string;
@@ -86,26 +23,8 @@ type Farm = {
   manager: string;
 };
 
-type FarmLookup = Record<
-  string,
-  {
-    periodEnd: number;
-    rewardRate: number;
-    rewardBalance: number;
-    stakingToken: string;
-    owner: string;
-    rewardsDistribution: string;
-    lastReward: string;
-  }
->;
-
 // == CONSTANTS ==
 const SECONDS_PER_WEEK = 60 * 60 * 24 * 7;
-const tokenName: Record<string, string> = {
-  [Token.CELO]: "CELO",
-  [Token.POOF]: "POOF",
-  [Token.UBE]: "UBE",
-};
 
 const Rewards: React.FC = () => {
   const router = useRouter();
@@ -178,6 +97,7 @@ const Rewards: React.FC = () => {
       owner,
       rewardsDistribution,
       lastReward,
+      rewardTokenSymbol,
     ] = await Promise.all([
       farmContract.periodFinish(),
       farmContract.rewardRate(),
@@ -188,6 +108,7 @@ const Rewards: React.FC = () => {
       farmContract
         .queryFilter(farmContract.filters.RewardAdded(null))
         .then((events) => events.reverse()[0]?.args.reward),
+      rewardToken.symbol(),
     ]);
     return {
       periodEnd: Number(periodEnd),
@@ -197,6 +118,7 @@ const Rewards: React.FC = () => {
       owner,
       rewardsDistribution,
       lastReward: lastReward?.toString() || "0",
+      rewardTokenSymbol,
     };
   }, [farm, provider]);
   const [lookup, refetchLookup] = useAsyncState(null, lookupCall);
@@ -230,14 +152,12 @@ const Rewards: React.FC = () => {
             )}
             {lookup.rewardRate && (
               <Text sx={{ display: "block" }}>
-                Rewards per week: {lookup.rewardRate}{" "}
-                {tokenName[farm.rewardToken]}
+                Rewards per week: {lookup.rewardRate} {lookup.rewardTokenSymbol}
               </Text>
             )}
             {lookup.rewardBalance && (
               <Text sx={{ display: "block" }}>
-                Farm balance: {lookup.rewardBalance}{" "}
-                {tokenName[farm.rewardToken]}
+                Farm balance: {lookup.rewardBalance} {lookup.rewardTokenSymbol}
               </Text>
             )}
             {lookup.stakingToken && (
@@ -266,8 +186,7 @@ const Rewards: React.FC = () => {
               }}
               mr={1}
             >
-              Transfer {fromWei(lookup.lastReward)}{" "}
-              {tokenName[farm.rewardToken]}
+              Transfer {fromWei(lookup.lastReward)} {lookup.rewardTokenSymbol}
             </Button>
             <Button
               onClick={() => {
@@ -276,12 +195,12 @@ const Rewards: React.FC = () => {
               }}
               mr={1}
             >
-              Notify {fromWei(lookup.lastReward)} {tokenName[farm.rewardToken]}
+              Notify {fromWei(lookup.lastReward)} {lookup.rewardTokenSymbol}
             </Button>
             <Button
               onClick={() => {
                 const amount = prompt(
-                  `Enter amount of ${tokenName[farm.rewardToken]} to transfer`
+                  `Enter amount of ${lookup.rewardTokenSymbol} to transfer`
                 );
                 if (!amount) {
                   console.warn("Invalid amount");
@@ -292,12 +211,12 @@ const Rewards: React.FC = () => {
               }}
               mr={1}
             >
-              Transfer custom {tokenName[farm.rewardToken]}
+              Transfer custom {lookup.rewardTokenSymbol}
             </Button>
             <Button
               onClick={() => {
                 const amount = prompt(
-                  `Enter amount of ${tokenName[farm.rewardToken]} to notify`
+                  `Enter amount of ${lookup.rewardTokenSymbol} to notify`
                 );
                 if (!amount) {
                   console.warn("Invalid amount");
@@ -308,7 +227,7 @@ const Rewards: React.FC = () => {
               }}
               mr={2}
             >
-              Notify custom {tokenName[farm.rewardToken]}
+              Notify custom {lookup.rewardTokenSymbol}
             </Button>
           </Box>
         </Card>
